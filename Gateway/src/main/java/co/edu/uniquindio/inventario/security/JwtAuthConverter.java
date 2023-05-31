b3ccbbc4e6051c031ca71644d51d3602ee8f6050
roaco <roaco_91@hotmail.com>
2023-05-30 21:27:38 -0500

    Se actualiza proyecto configurando las rutas del microservicio de inventario, solicitando asi la autenticacion por medio de gateway

---
+++ Gateway/src/main/java/co/edu/uniquindio/inventario/security/JwtAuthConverter.java
@@ -0,0 +1,61 @@
+package co.edu.uniquindio.inventario.security;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+public class JwtAuthConverter implements Converter<Jwt, Mono<AbstractAuthenticationToken>> {
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    @Value("${jwt.auth.converter.resource-id}")
+    private String resourceId;
+
+    @Value("${jwt.auth.converter.principal-attribute}")
+    private String principalAttribute;
+    @Override
+    public Mono<AbstractAuthenticationToken> convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()).collect(Collectors.toSet());
+        return Mono.just(new JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt)));
+    }
+    private String getPrincipalClaimName(Jwt jwt) {
+        String claimName = JwtClaimNames.SUB;
+        if (principalAttribute != null) {
+            claimName = principalAttribute;
+        }
+        return jwt.getClaim(claimName);
+    }
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+        if (resourceAccess == null
+                || (resource = (Map<String, Object>) resourceAccess.get(resourceId)) == null
+                || (resourceRoles = (Collection<String>) resource.get("roles")) == null) {
+            return Set.of();
+        }
+        return resourceRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
+    }
+}
+
+
